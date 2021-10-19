244b039959d3c76f6a9d105c3cdec94a8ad04365
manish <manishkmar772@gmail.com>
2021-10-19 16:37:30 +0530

    hello

---
+++ mk.java
@@ -0,0 +1,12 @@
+package com.Demo;
+
+public class mk {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        int a=10;
+        int b=20;
+        System.out.println(a+b);
+	}
+
+}
